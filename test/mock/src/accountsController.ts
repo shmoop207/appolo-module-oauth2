606e39cc41b895381099e14b59c35f5982b09d96
roman <shmoop207@gmail.com>
2020-02-03 15:50:36 +0200

    first

--- test/mock/src/accountsController.ts
+++ test/mock/src/accountsController.ts
@@ -18,7 +18,7 @@ export class AccountsController extends Controller {
                 clientId: name,
                 clientSecret: pass,
                 scope: req.query.scope
-            })
+            });
 
             return token;
 
